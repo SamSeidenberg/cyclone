5be92a97c1b436a0f718aedc766b3321a6d80538
Samuel Seidenberg <seidensj@gmail.com>
2019-12-14 12:00:18 -0500

    Updates

---
+++ precache-manifest.4ab893784fd5ca931f19ae4923c661d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d557affafab8a64cf3b2222011e3c",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/index.html"
+  },
+  {
+    "revision": "97e5bfcd8100af3d2221",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd6f125bfb59a0bae6f5",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/js/2.98a39b5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/js/2.98a39b5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97e5bfcd8100af3d2221",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/js/main.5cb22696.chunk.js"
+  },
+  {
+    "revision": "664e96e7aa86990f3b58",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/js/runtime-main.d56594cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SamSeidenberg/SamSeidenberg.github.io/cyclone/static/media/logo.5d5d9eef.svg"
+  }
+]);
